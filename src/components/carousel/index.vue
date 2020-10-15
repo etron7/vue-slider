2d85de95fe6977cbe4d24b80bb58ec1229b74ee2
etron7 <iquark10@gmail.com>
2020-10-15 13:17:28 +0300

    first commit

---
+++ src/components/carousel/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="carousel">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'Carousel',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
