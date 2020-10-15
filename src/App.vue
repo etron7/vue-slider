c4f7398beb5627eace8c3ab9dabd57b1afd8ad2b
etron7 <32978099+etron7@users.noreply.github.com>
2020-10-15 13:25:55 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+  <!-- <h1>Slider app</h1> -->
+  <Carousel 
+  :carousel_data="sliderItems"
+  :interval="2000"
+  />
+  </div>
+</template>
+
+<script>
+import Carousel from './components/carousel'
+
+export default {
+  name: 'App',
+  components: {
+    Carousel
+  },
+  data() {
+    return {
+      sliderItems: [
+        {id: 1, title:'slide1', filename: '1.jpg'},
+        {id: 2, title:'slide2', filename: '2.jpg'},
+        {id: 3, title:'slide3', filename: '3.jpg'},
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+p{
+  color: #000;
+}
+</style>
